9b919723400fef5fa5e3e12eb7b7b03395b2b2f7
Sági Péter <psagi88developer@gmail.com>
2019-05-08 12:09:57 +0200

    Initial Commit

---
+++ miniweather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miniweather
+//
+//  Created by Péter Sági on 2019. 05. 08..
+//  Copyright © 2019. Péter Sági. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
